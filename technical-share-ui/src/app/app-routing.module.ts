e34f621c59c821082f78f6e7942af7cb4b60891f
Luany Oliveira <luanyoliveira16@gmail.com>
2022-04-10 19:47:34 -0300

    criando página de perfil

--- technical-share-ui/src/app/app-routing.module.ts
+++ technical-share-ui/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full' ,redirectTo: 'usuarios'},
   { path: 'home', component: HomeComponent},
+  { path: 'perfil', component: PerfilComponent},
+  
   
   {
     path: 'usuarios',
--- technical-share-ui/src/app/app.module.ts
+++ technical-share-ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
